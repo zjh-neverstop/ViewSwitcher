03a27cfd67934c26f825ca0ab041fbb4c9734e99
zjh-neverstop <zhaojinghao1986@126.com>
2014-11-06 13:50:11 +0800

    rewrite ViewSwitcher

--- ViewSwitcher.js
+++ ViewSwitcher.js
@@ -1,254 +1,68 @@
 /**
- * Created by zhaojinghao on 14-9-24.
+ * Created by Administrator on 14-11-6.
  */
+(function(window,undefined){
 
-//内容轮播器
-(function(window) {
+    //ViewSwitcher全局变量区域
 
-    var ViewSwitcher = (function() {
+    var animata = function(direction){
 
-        //view模版
-        var tpl = document.getElementById("tpl_table").innerHTML;
+    };
 
-        //相对屏幕居中显示
-        function makeCenter(id) {
-            document.getElementById(id).style.display = "block";
-            $('#' + id).css("top", Math.max(0, (($(window).height() - $('#' + id).outerHeight()) / 2) + $(window).scrollTop()) + "px");
-            $('#' + id).css("left", Math.max(0, (($(window).width() - $('#' + id).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+    var View = (function(){
+
+
+
+        var view = function(){
+            this.curPosition = ""; //curPosition:"top","right","bottom","left","center"
+            this.direction = "";
+            this.visible = false;
+
         }
 
-        //相对父元素居中
-        function makeCenterInParent(id) {
-            document.getElementById(id).style.display = "block";
-            var obj = $("#" + id);
-            var parent = obj.parent();
-            var parentWidth = parent.width();
-            var parentHeight = parent.height();
-            var selfWidth = obj.width();
-            var selfHeight = obj.height();
-            var left = (parentWidth - selfWidth) / 2;
-            var top = (parentHeight - selfHeight) / 2;
-            $('#' + id).css("top", top + "px");
-            $('#' + id).css("left", left + "px");
-        }
-        
-        //相对某个元素居中
-        function makeCenterInElement(id, targetEle) {
-            document.getElementById(id).style.display = "block";
-            var obj = $("#" + id);
-            var target = $(targetEle);
-            var targetWidth = target.width();
-            var targetHeight = target.height();
-            var selfWidth = obj.width();
-            var selfHeight = obj.height();
-            var left = target.get(0).offsetLeft + (targetWidth - selfWidth) / 2;
-            var top = target.get(0).offsetTop + (targetHeight - selfHeight) / 2;
-            $('#' + id).css("top", top + "px");
-            $('#' + id).css("left", left + "px");
-            console.debug(document.getElementById(id).style.display);
-            console.debug("left:" + left + "top:" + top);
-        }
-        
-        var ViewSwitcher = function() {
-            //属性定义
-            this.curIndex = 0;
-            this.pids = [];
-            this.viewOneId = "";
-            this.viewTwoId = "";
+        view.prototype = {
+            show:function(){
 
-            this.cbCollection = []; //额外属性，用来控制所选checkbox的状态
+            },
+            hidden:function(){
 
-            //事件定义
-            this.beforeViewChange = null; //轮换view前调用，需要自己实现
-            this.afterViewChange = null; //轮换view后调用，需要自己实现
-            this.beforeFinish = null;    //轮换结束时调用，需要自己实现
+            }
+        };
+        return view;
+    })();
+
+
+    var ViewSwitcher = (function(){
+
+        var ViewSwitcher = function(){
+            return new ViewSwitcher.fn.init();
         };
 
-        ViewSwitcher.prototype = {
-            //初始化轮播器
-            init: function() {
-
-                var view1 = document.getElementById(this.viewOneId);
-                var view2 = document.getElementById(this.viewTwoId);
-                view1.innerHTML = "";
-                view2.innerHTML = "";
-                view1.style.left = "0px";
-                view2.style.left = "600px";
-
-                if (this.pids.length == 0) {
-                    this.onFinish();
-                    return false;
-                }
-
-                var switchObj = this;
-
-                this.curIndex = 0;
-                var pid = this.pids[this.curIndex];
-                makeCenter("ajax-loader");
-                this.getDataForView(this.viewOneId, makeCenter, "carousel");
-            },
-            //轮换view
-            switchView: function() {
-                
-                //调用切换view开始事件
-                this.onViewSwitching();
-
-                this.curIndex++;
-
-                if (this.curIndex >= this.pids.length || this.curIndex < 0) {
-
-                    this.onFinish();
-
-                    return false;
-                }
-
-                var switchObj = this;
-
-                var viewOne = $("#" + this.viewOneId);
-                var viewTwo = $("#" + this.viewTwoId);
-
-                var btn = $("#confirmCheckBtn");
-
-                btn.attr("disabled", "disabled");
-
-                var viewId = parseInt(this.curIndex) % 2 == 0 ? this.viewOneId : this.viewTwoId;
-
-                this.getDataForView(viewId, function() {
-                    viewOne.animate({ "left": "-=600px" }, 500, function() {
-
-                        if (parseInt(viewOne.css("left")) < 0) {
-                            viewOne.hide();
-                            viewOne.animate({ "left": "600px" }, 10);
-                            viewOne.show();
-                            btn.removeAttr("disabled");
-                        }
-                    });
-                    viewTwo.animate({ "left": "-=600px" }, 500, function() {
-                        if (parseInt(viewTwo.css("left")) < 0) {
-                            viewTwo.hide();
-                            viewTwo.animate({ "left": "600px" }, 10);
-                            viewTwo.show();
-                            btn.removeAttr("disabled");
-                        }
-                    });
-
-                    //调用切换view结束事件
-                    switchObj.onViewSwitched();
-
-                });
-
-            },
-            //切换view开始事件
-            onViewSwitching: function() {
-                if (typeof (this.beforeViewChange) === 'function') {
-                    try {
-                        this.afterViewChange();
-                    }
-                    catch (e) {
-
-                    }
-                }
-            },
-            //切换view结束事件
-            onViewSwitched: function() {
-                if (typeof (this.afterViewChange) === 'function') {
-                    try {
-                        this.afterViewChange();
-                    }
-                    catch (e) {
-
-                    }
-                }
-            },
-            //轮播结束处理事件
-            onFinish: function() {
-
-                if (typeof (this.beforeFinish) === 'function') {
-
-                    try {
-                        this.beforeFinish();
-                    }
-                    catch (e) {
-
-                    }
-
-                }
-
-                this.resetStatus();
-
-            },
-            resetStatus: function() {
-                //重置轮播器状态
-                this.curIndex = -1;
-                this.pids = [];
-                var view1 = document.getElementById(this.viewOneId);
-                var view2 = document.getElementById(this.viewTwoId);
-                view1.innerHTML = "";
-                view2.innerHTML = "";
-                view1.style.left = "0px";
-                view2.style.left = "600px";
-
-                document.getElementById("carousel").style.display = "none";
-            },
-            /*
-            给指定的view填充数据
-            可以将数据获取部分定义出来供调用者自己实现，同时定义好返回数据的格式，
-            这样既能够达到获取数据方式的多样化，还可以保证以定义好的格式返回数据，不过这样需要在该类中添加一个格式验证方法
-            日后再优化吧
-            */
-            getDataForView: function(viewId, callback, param) {
-
-                var switchObj = this;
-                var pid = this.pids[this.curIndex];
-
-                $.ajax({
-                    type: "POST",
-                    url: "UploadHandler.ashx",
-                    data: {
-                        uncheckpid: pid,
-                        action: "GetConfirmInfo"
-                    },
-                    dataType: "json",
-                    success: function(result) {
-
-                        //switchObj.curIndex++;
-                        console.debug("getDataForView success:" + switchObj.curIndex);
-
-                        if (result.status == "success") {
-                            var html = Mustache.render(tpl, result.data);
-                            document.getElementById(viewId).innerHTML = html;
-                            callback(param);
-                        }
-                        else {
-                            alert(result.message);
-                        }
-
-                    },
-                    beforeSend: function() {
-                        //console.debug("beforeSend");
-                        //makeCenterInParent("ajax-loader");
-                        if (document.getElementById("carousel").style.display == "block") {
-                            makeCenterInElement("ajax-loader", document.getElementById("carousel"));
-                        }
-                    },
-                    complete: function() {
-                        console.debug("complete");
-                        $("#ajax-loader").hide();
-
-                    },
-                    error: function() {
-
-                    }
-                });
+        ViewSwitcher.fn = ViewSwitcher.prototype = {
+            //轮播速度
+            speed : 1000,
+            //轮播方向
+            direction : "left",
+            //
+            width:0,
+            //
+            height:0,
+            datas:[],
+            views:[],
+            //构造函数
+            init : function(options){
+                this.speed = options.speed||1000;
+                this.direction = options.direction||"left";
+                this.datas = options.datas||[];
             }
         };
 
-        return function() {
-            return new ViewSwitcher();
-        };
+        //覆盖init的原型对象
+        ViewSwitcher.fn.init.prototype = ViewSwitcher.fn;
 
+        return ViewSwitcher;
     })();
 
     window.ViewSwitcher = ViewSwitcher;
 
-})(window, undefined);
+})(window);
